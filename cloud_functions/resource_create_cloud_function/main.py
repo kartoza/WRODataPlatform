785b0b833c703472c2da580eb887b54e51aa88d6
Mohab <homab3@gmail.com>
2022-10-06 15:15:54 +0200

    tests to resources created via GCS

--- cloud_functions/resource_create_cloud_function/main.py
+++ cloud_functions/resource_create_cloud_function/main.py
@@ -37,6 +37,9 @@ def ckan_gcs_resource_create(cloud_event):
             return
 
         package_name = get_package_name(resource_name)
+        if package_name == "":
+            return
+
         res_format = get_resource_format(resource_name)
         res_short_name = get_resource_short_name(resource_name)
         headers = {"Authorization":"6d5f1cf7-6d42-467f-b27f-b0a8454572c0"}
@@ -46,25 +49,38 @@ def ckan_gcs_resource_create(cloud_event):
             response = requests.post("https://data.waterresearchobservatory.org/api/3/action/resource_create",headers=headers, data=res)
             response_ob = response.json()
         except:
-            return "couldn't create ckan resource "
+            return "couldn't create ckan resource"
         response_result = response_ob.get("result")
-        resource_ckan_id = response_result.get("id")
-        resource_ckan_url = response_result.get("url") if response_result is not None else ""
-        # the following also calls rename_bucket_blob
-        underscord_resource_names = get_resource_underscored_url(resource_ckan_url)
-        resource_ckan_url = underscord_resource_names.get("")
-        patched_resource = {"id":resource_ckan_id, "url":resource_ckan_url}
-        response = requests.post("https://data.waterresearchobservatory.org/api/3/action/resource_patch",headers=headers, data=patched_resource)
-        new_blob_name = underscord_resource_names.get("new_blob_name")
-        bucket.rename_blob(blob, new_blob_name)
+        if response_result is not None:
+            resource_ckan_id = response_result.get("id")
+            resource_ckan_url = response_result.get("url") if response_result is not None else ""
+            # the following also calls rename_bucket_blob
+            underscord_resource_names = get_resource_underscored_url(resource_ckan_url)
+            resource_ckan_url = underscord_resource_names.get("updated_url")
+            patched_resource = {"id":resource_ckan_id, "url":resource_ckan_url}
+            response = requests.post("https://data.waterresearchobservatory.org/api/3/action/resource_patch",headers=headers, data=patched_resource)
+            new_blob_name = underscord_resource_names.get("new_blob_name")
+            bucket.rename_blob(blob, new_blob_name)
     
 
-def get_package_name(res_name:str)-> str:
+def get_package_name(res_name:str, auth:dict)-> str:
     """
     packages names are unique 
     """
+    slash_count = res_name.count("/")
+    if slash_count < 5:
+        return ""
+
+    response = requests.post("https://data.waterresearchobservatory.org/api/3/action/package_list", headers=auth)
+    res_ob = response.json()
+    packages = res_ob.get("result")
+    
     first_part = res_name.split("/",5)
     package_name = first_part[4]
+    
+    if package_name not in packages:
+        return ""
+
     return package_name
 
 def get_resource_short_name(res_name:str) -> dict:
@@ -79,7 +95,7 @@ def get_resource_short_name(res_name:str) -> dict:
     first_part, last_part = res_name.rsplit("/",1)
     return Path(last_part).stem
 
-def get_resource_underscored_url(res_url:str) -> dict:
+def get_resource_underscored_url(res_url:str, bucket_name="wrc_wro_datasets") -> dict:
     """
     extract the resource name, id and extension
     the resource name would be without
@@ -89,16 +105,18 @@ def get_resource_underscored_url(res_url:str) -> dict:
     -> resource_name
     """
     first_part, last_part = res_url.rsplit("/",1)
-    resource_name, id = last_part.split("_id_", 1)
+    resource_name, id_with_ext = last_part.split("_id_", 1)
     res_short_name = resource_name.replace(" ","_")
-    name_with_id = res_short_name+"_id_"+id
+    name_with_id = res_short_name+"_id_"+id_with_ext
     updated_url = first_part+"/"+ name_with_id
-    new_blob_name = remove_url_domain(first_part) + "/" + name_with_id
+    removed_domain = remove_url_domain(first_part, bucket_name)
+    new_blob_name = removed_domain  + "/" + name_with_id
     return {"updated_url":updated_url, "new_blob_name":new_blob_name}
 
 
 def get_resource_format(resource_name):
-    return Path(resource_name).suffix
+    ext = Path(resource_name).suffix
+    return ext[1:]
 
 def build_resource_url(res_name:str) -> str:
     """
@@ -109,9 +127,10 @@ def build_resource_url(res_name:str) -> str:
     """
     return "https://storage.cloud.google.com/wrc_wro_datasets/"+res_name
 
-def remove_url_domain(resource_url:str):
+def remove_url_domain(resource_url:str, bucket_name="wrc_wro_datasets"):
     """
     removes storage.google.com
     from the path
     """
-    return resource_url.replace("https://storage.cloud.google.com/wrc_wro_datasets/","")
+    new_name = resource_url.replace(f"https://storage.cloud.google.com/{bucket_name}/","")
+    return new_name
